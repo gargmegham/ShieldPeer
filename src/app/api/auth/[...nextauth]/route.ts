0fb04054ac297d7493c7ed0838c0f4c2165a82b2
Megham Garg <gargmegham3@gmail.com>
2024-06-03 18:36:54 +0530

    remove prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../../libs/prismaClient";
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -10,7 +8,6 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     }),
   ],
-  adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
@@ -29,9 +26,7 @@ const authOptions: NextAuthOptions = {
     },
   },
   pages: {
-    signIn: "/auth/signin",
-    error: "/auth/error",
-    signOut: "/auth/signout",
+    signIn: "/auth",
   },
 };
 
