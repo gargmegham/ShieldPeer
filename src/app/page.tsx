dad85f75fcc9786386e1acd5bf3da0ebe5cab899
Megham Garg <gargmegham3@gmail.com>
2024-06-03 14:20:56 +0530

    chore: add header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <main className="">
+      <Header />
+      <Footer />
     </main>
   );
 }
