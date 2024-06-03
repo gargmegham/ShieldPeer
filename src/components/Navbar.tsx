88dd4fc74897e403bb04813fa1aea08a1f8424c2
Megham Garg <gargmegham3@gmail.com>
2024-06-03 14:47:41 +0530

    chore: Remove unnecessary links from Navbar and update icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,28 +5,8 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { cn } from "@/utils/cn";
 import { AiOutlineHome } from "react-icons/ai";
-import { GrProjects } from "react-icons/gr";
 import { HiOutlineOfficeBuilding } from "react-icons/hi";
-
-const navItems = [
-  {
-    name: "Pricing",
-    link: "#pricing",
-    icon: <AiOutlineHome className="size-4 text-neutral-500 dark:text-white" />,
-  },
-  {
-    name: "Demo",
-    link: "#demo",
-    icon: <GrProjects className="size-4 text-neutral-500 dark:text-white" />,
-  },
-  {
-    name: "Support",
-    link: "/support",
-    icon: (
-      <HiOutlineOfficeBuilding className="size-4 text-neutral-500 dark:text-white" />
-    ),
-  },
-];
+import { MdOutlinePriceChange, MdOutlineSupport } from "react-icons/md";
 
 const Navbar = () => {
   const router = useRouter();
@@ -46,20 +26,43 @@ const Navbar = () => {
         }}
         className="flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4"
       >
-        {navItems.map((navItem, idx) => (
-          <button
-            key={`link=${idx}`}
-            onClick={() => {
-              router.push(navItem.link);
-            }}
-            className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
-            )}
-          >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
-          </button>
-        ))}
+        <button
+          onClick={() => {
+            router.push("/");
+          }}
+          className={cn(
+            "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+          )}
+        >
+          <span className="block sm:hidden">
+            <AiOutlineHome className="size-4 text-neutral-500 dark:text-white" />
+          </span>
+          <span className="hidden sm:block text-sm">Home</span>
+        </button>
+        <button
+          onClick={() => {
+            router.push("/#pricing");
+          }}
+          className={cn(
+            "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+          )}
+        >
+          <span className="block sm:hidden">
+            <MdOutlinePriceChange className="size-4 text-neutral-500 dark:text-white" />
+          </span>
+          <span className="hidden sm:block text-sm">Pricing</span>
+        </button>
+        <a
+          href="mailto:business@meghamgarg.com"
+          className={cn(
+            "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+          )}
+        >
+          <span className="block sm:hidden">
+            <MdOutlineSupport className="size-4 text-neutral-500 dark:text-white" />
+          </span>
+          <span className="hidden sm:block text-sm">Support</span>
+        </a>
         <button
           onClick={() => {}}
           className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full"
