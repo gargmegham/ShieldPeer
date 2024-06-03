2da335d90bf778cc315a5213f63e9f5f4cd748e0
Megham Garg <gargmegham3@gmail.com>
2024-06-03 14:36:20 +0530

    chore: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="">
-      <Header />
+      <Navbar />
       <Footer />
     </main>
   );
---
+++ src/utils/cn.ts
@@ -0,0 +1,11 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * @description A utility function to merge Tailwind CSS classes with clsx
+ * @param  {...any} inputs
+ * @returns
+ */
+export function cn(...inputs: any[]) {
+  return twMerge(clsx(inputs));
+}
