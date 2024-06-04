b7f2e8e96fb3a6cdd4197a030b0035f9d3d5a804
Megham Garg <gargmegham3@gmail.com>
2024-06-04 10:37:02 +0530

    chore: refactor navbar, add navbar to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,21 @@
 "use client";
 
 import React from "react";
-import { motion } from "framer-motion";
-import { LampContainer } from "@/components/Lamp";
+import Navbar from "@/components/Landing/Navbar";
+import { MdOutlineInventory, MdOutlineDocumentScanner } from "react-icons/md";
 
-export default function LampDemo() {
+export default function Dashboard() {
   return (
-    <LampContainer>
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-      >
-        Coming soon
-      </motion.h1>
-    </LampContainer>
+    <main className="" id="dashboard">
+      <Navbar
+        items={[
+          { label: "Inventory", link: "/inventory", icon: MdOutlineInventory },
+          { label: "Logs", link: "/logs", icon: MdOutlineDocumentScanner },
+        ]}
+        ctaLabel="Settings"
+        ctaLink="/settings"
+        logoLink="/"
+      />
+    </main>
   );
 }
