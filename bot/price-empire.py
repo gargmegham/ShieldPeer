26e28fac63a9e5bd291c71a9c926d8cbb99ff625
Megham Garg <gargmegham3@gmail.com>
2024-06-05 16:17:06 +0530

    refactor: Add Dockerfile and logger module fot python bot

---
+++ bot/price-empire.py
@@ -0,0 +1,12 @@
+import time
+
+from logger import get_logger
+
+if __name__ == '__main__':
+    logger = get_logger()
+    try:
+        logger.info('Custom service started')
+        while True:
+            time.sleep(2)
+    finally:
+        logger.info('Custom service stopped')
