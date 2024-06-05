3c1d5fe2116276865f39b55595560acc7178c51c
Megham Garg <gargmegham3@gmail.com>
2024-06-05 17:59:29 +0530

    chore: Add new interfaces for Item, ItemSetting, Listing, and Log

--- src/types/database.ts
+++ src/types/database.ts
@@ -13,6 +13,7 @@ export interface Setting {
 
 export interface PriceRange {
   id: string;
+  user_id: string;
   source_price_min: number;
   source_price_max: number;
   listing_price_min: number;
@@ -20,4 +21,54 @@ export interface PriceRange {
   listing_price_if_no_one_to_undercut: number;
   when_no_one_to_undercut_list_at: string;
   always_undercut_by_percentage_if_listing_price_is_greater_than: number;
+}
+
+export interface Item {
+  id: string;
+  user_id: string;
+  waxpeer_id: string;
+  name: string;
+  type: string;
+  condition: string;
+  price: number;
+  created_at: string;
+  is_active: boolean;
+  price_updated_at: string;
+  condition_updated_at: string;
+  meta_data: object;
+}
+
+export interface ItemSetting {
+  id: string;
+  user_id: string;
+  created_at: string;
+  undercut_by_price?: number;
+  undercut_by_percentage?: number;
+  undercut_by?: string;
+  listing_price_min: number;
+  listing_price_max: number;
+  listing_price_if_no_one_to_undercut: number;
+  when_no_one_to_undercut_list_at: string;
+  always_undercut_by_percentage_if_listing_price_is_greater_than: number;
+  item_id: Item["id"];
+}
+
+export interface Listing {
+  id: string;
+  user_id: string;
+  item_id: Item["id"];
+  price: number;
+  created_at: string;
+  updated_at?: string;
+  meta_data?: object;
+}
+
+export interface Log {
+  id: string;
+  item_id: Item["id"];
+  user_id: string;
+  created_at: string;
+  type: string;
+  message: string;
+  meta_data?: object;
 }
