a4bee116b488b30b7341cc835692f7a188db682d
Megham Garg <gargmegham3@gmail.com>
2024-06-05 15:09:01 +0530

    feat: Add Inventory and Logs pages

---
+++ src/app/inventory/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import Navbar from "@/components/ui/navbar";
+import { MdOutlineInventory, MdOutlineDocumentScanner } from "react-icons/md";
+
+export default function Inventory() {
+  return (
+    <main
+      className="bg-black min-h-screen bg-grid-small-white/30"
+      id="dashboard"
+    >
+      <Navbar
+        items={[
+          { label: "Inventory", link: "/inventory", icon: MdOutlineInventory },
+          { label: "Logs", link: "/logs", icon: MdOutlineDocumentScanner },
+        ]}
+        ctaLabel="Settings"
+        ctaLink="/settings"
+        logoLink="/"
+      />
+    </main>
+  );
+}
