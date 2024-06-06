21030da5f884d5048a24c5ae69ae886a34f81d7a
Megham Garg <gargmegham3@gmail.com>
2024-06-06 16:36:04 +0530

    refactor: Add tooltip for active status

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -135,81 +135,96 @@ export default function Inventory() {
                           true
                       )
                     : inventory
-                ).map((item) => (
-                    <Card key={item.asset_id} className="relative py-6">
-                        <div className="px-6 flex items-center text-xs justify-between">
-                            {item.is_active ? (
-                                <div className="flex items-center gap-x-2">
-                                    <CgMediaLive className="text-green-300 animate-pulse" />
-                                    Active
+                )
+                    .sort((a, b) => (a.price > b.price ? -1 : 1))
+                    .map((item) => (
+                        <Card key={item.asset_id} className="relative py-6">
+                            <div className="px-6 flex items-center text-xs justify-between">
+                                <TooltipProvider>
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            {item.is_active ? (
+                                                <div className="flex items-center gap-x-2">
+                                                    <CgMediaLive className="text-green-300 animate-pulse" />
+                                                    Active
+                                                </div>
+                                            ) : (
+                                                <div className="flex items-center gap-x-2">
+                                                    <FaRegPauseCircle className="text-red-500" />
+                                                    Inactive
+                                                </div>
+                                            )}
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                            <p>
+                                                {item.is_active
+                                                    ? "Item is currently maintained by the bot"
+                                                    : "Item will not be listed by the bot"}
+                                            </p>
+                                        </TooltipContent>
+                                    </Tooltip>
+                                </TooltipProvider>
+                                <TooltipProvider>
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            <div className="rounded-full border border-yellow-600/30 p-1">
+                                                <SiGunicorn color={item.rarity_color} className="size-4" />
+                                            </div>
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                            <p>Rarity Color</p>
+                                        </TooltipContent>
+                                    </Tooltip>
+                                </TooltipProvider>
+                            </div>
+                            <CardContent className="pt-2 pb-4">
+                                <div className="flex justify-center items-center">
+                                    <Image
+                                        src={`https://community.cloudflare.steamstatic.com/economy/image/${item.image}`}
+                                        alt="Waxpeer"
+                                        className="bg-neutral-800 rounded-lg p-2"
+                                        loading="lazy"
+                                        width={245}
+                                        height={185}
+                                    />
                                 </div>
-                            ) : (
-                                <div className="flex items-center gap-x-2">
-                                    <FaRegPauseCircle className="text-red-500" />
-                                    Inactive
-                                </div>
-                            )}
-                            <TooltipProvider>
-                                <Tooltip>
-                                    <TooltipTrigger>
-                                        <div className="rounded-full border border-yellow-600/30 p-1">
-                                            <SiGunicorn color={item.rarity_color} className="size-4" />
+                            </CardContent>
+                            <CardHeader className="relative py-0">
+                                <CardTitle className="truncate pr-4 text-sm text-neutral-300">
+                                    {item?.name ?? item.market_hash_name}
+                                </CardTitle>
+                                <CardDescription>{item?.exterior ?? "N/A"}</CardDescription>
+                            </CardHeader>
+                            <div className="px-6 mt-2 space-y-1">
+                                {setting.price_empire_source && (
+                                    <div className="flex gap-x-2">
+                                        <div className="text-neutral-500 underline decoration-wavy">
+                                            {setting.price_empire_source[0].toLocaleUpperCase()}
+                                            {setting.price_empire_source.slice(1)}
                                         </div>
-                                    </TooltipTrigger>
-                                    <TooltipContent>
-                                        <p>Rarity Color</p>
-                                    </TooltipContent>
-                                </Tooltip>
-                            </TooltipProvider>
-                        </div>
-                        <CardContent className="pt-2 pb-4">
-                            <div className="flex justify-center items-center">
-                                <Image
-                                    src={`https://community.cloudflare.steamstatic.com/economy/image/${item.image}`}
-                                    alt="Waxpeer"
-                                    className="bg-neutral-800 rounded-lg p-2"
-                                    loading="lazy"
-                                    width={245}
-                                    height={185}
-                                />
-                            </div>
-                        </CardContent>
-                        <CardHeader className="relative py-0">
-                            <CardTitle className="truncate pr-4 text-sm text-neutral-300">
-                                {item?.name ?? item.market_hash_name}
-                            </CardTitle>
-                            <CardDescription>{item?.exterior ?? "N/A"}</CardDescription>
-                        </CardHeader>
-                        <div className="px-6 mt-2 space-y-1">
-                            {setting.price_empire_source && (
-                                <div className="flex gap-x-2">
-                                    <div className="text-neutral-500 underline decoration-wavy">
-                                        {setting.price_empire_source[0].toLocaleUpperCase()}
-                                        {setting.price_empire_source.slice(1)}
+                                        <div className="font-extrabold text-neutral-200 font-bricolage">
+                                            {item.price.toLocaleString("en-US", {
+                                                style: "currency",
+                                                currency: "USD",
+                                            })}
+                                        </div>
                                     </div>
-                                    <div className="font-extrabold text-neutral-200 font-bricolage">
-                                        {item.price.toLocaleString("en-US", {
-                                            style: "currency",
-                                            currency: "USD",
-                                        })}
+                                )}
+                                <div className="flex gap-x-2 text-sm">
+                                    <div className="text-neutral-500">Float</div>
+                                    <div className="font-extrabold text-neutral-200">
+                                        {item?.float?.toFixed(5) ?? "N/A"}
                                     </div>
                                 </div>
-                            )}
-                            <div className="flex gap-x-2 text-sm">
-                                <div className="text-neutral-500">Float</div>
-                                <div className="font-extrabold text-neutral-200">
-                                    {item?.float?.toFixed(5) ?? "N/A"}
-                                </div>
                             </div>
-                        </div>
-                        <div className="px-6 mt-4 w-full">
-                            <Button className="w-full relative" size="lg" variant="outline">
-                                Settings
-                                <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-amber-500 to-transparent h-px" />
-                            </Button>
-                        </div>
-                    </Card>
-                ))}
+                            <div className="px-6 mt-4 w-full">
+                                <Button className="w-full relative" size="lg" variant="outline">
+                                    Settings
+                                    <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-amber-500 to-transparent h-px" />
+                                </Button>
+                            </div>
+                        </Card>
+                    ))}
             </div>
         </main>
     )
