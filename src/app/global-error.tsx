7f1614966a48d2b026b438e0a713532acc44ebb6
Megham Garg <gargmegham3@gmail.com>
2024-06-06 11:57:55 +0530

    feat: Add GlobalError and NotFound components

---
+++ src/app/global-error.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import Link from "next/link"
+
+import React from "react"
+
+import { AiOutlineHome } from "react-icons/ai"
+
+import { SparklesCore } from "@/components/ui/sparkles"
+
+export default function GlobalError() {
+    return (
+        <div className="bg-black flex flex-col items-center justify-center overflow-hidden rounded-md relative h-screen">
+            <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">500</h1>
+            <div className="w-[40rem] h-40 relative">
+                {/* Gradients */}
+                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-yellow-500 to-transparent h-[2px] w-3/4 blur-sm" />
+                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-yellow-500 to-transparent h-px w-3/4" />
+                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-amber-500 to-transparent h-[5px] w-1/4 blur-sm" />
+                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-amber-500 to-transparent h-px w-1/4" />
+                {/* Core component */}
+                <SparklesCore
+                    background="transparent"
+                    minSize={0.4}
+                    maxSize={1}
+                    particleDensity={1200}
+                    className="w-full h-full"
+                    particleColor="#FFFFFF"
+                />
+
+                {/* Radial Gradient to prevent sharp edges */}
+                <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+            <Link
+                href="/home"
+                className="flex bg-black mt-8 border text-sm font-medium border-white/[0.2] text-white px-4 py-2 rounded-full relative items-center"
+            >
+                <AiOutlineHome className="mr-2 size-4" />
+                <span>Home</span>
+                <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-amber-500 to-transparent h-px" />
+            </Link>
+        </div>
+    )
+}
