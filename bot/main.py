2689f049fda1a57a5c3b7fd9eae19d92573bab1f
Megham Garg <gargmegham3@gmail.com>
2024-06-08 03:04:11 +0530

    refactor: Update bot to use asyncio for improved performance and maintainability

--- bot/main.py
+++ bot/main.py
@@ -1,18 +1,20 @@
 import asyncio
 
-from utils.bot import bot
-from utils.cron import cron
 from utils.logger import get_logger
 
+
+async def bot():
+    logger = get_logger("bot")
+
+
 if __name__ == "__main__":
-    logger = get_logger("main")
+    logger = get_logger("bot")
     try:
-        logger.info("ShieldPeer service started")
-        asyncio.run(cron())
+        logger.info("ShieldPeer Bot: service started")
         asyncio.run(bot())
     except KeyboardInterrupt:
-        logger.info("ShieldPeer service stopped")
+        logger.info("ShieldPeer Bot: service stopped")
     except Exception as err:
-        logger.error(f"An error occurred: {err}")
+        logger.error(f"ShieldPeer Bot: {err}")
     finally:
-        logger.info("ShieldPeer service stopped")
+        logger.info("ShieldPeer Bot: service stopped")
