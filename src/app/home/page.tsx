314d8451b029d973fb14eda30dffa89b9661abfe
Megham Garg <gargmegham3@gmail.com>
2024-06-07 13:22:20 +0530

    refactor: Add GitHub star link to home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import Link from "next/link"
+
+import { GitHubLogoIcon } from "@radix-ui/react-icons"
+
 import Navbar from "@/components/ui/navbar"
 
 import FAQ from "@/components/Landing/FAQ"
@@ -10,13 +14,22 @@ import Problem from "@/components/Landing/Problem"
 
 export default function Home() {
     return (
-        <main id="home">
+        <main id="home" className="relative">
             <Navbar />
             <Hero />
             <Problem />
             <Pricing />
             <FAQ />
             <Footer />
+            <Link
+                href="https://github.com/gargmegham/ShieldPeer"
+                target="_blank"
+                className="right-4 p-2 bottom-4 border text-sm font-medium border-white/[0.2] text-white rounded-xl bg-black flex items-center justify-center fixed"
+            >
+                <GitHubLogoIcon className="w-5 h-5 mr-2" />
+                <span>Star Us On Github</span>
+                <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-amber-500 to-transparent h-px" />
+            </Link>
         </main>
     )
 }
