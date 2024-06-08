80ef8468b18e4eb2e6fdf895ef719b8b30184157
Megham Garg <gargmegham3@gmail.com>
2024-06-08 06:33:25 +0530

    ```text refactor: Update middleware to redirect unauthorized users to /home
    This commit updates the middleware in the `middleware.ts` file to redirect unauthorized users to the `/home` page. Previously, the middleware only redirected users if they were not logged in, but it did not handle cases where users were logged in but not authorized to access certain paths. By adding the redirect logic, this change improves the security and user experience of the application by ensuring that unauthorized users are redirected to the appropriate page.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,29 +2,26 @@ import { NextRequest, NextResponse } from "next/server"
 
 import { getSupabaseClient } from "@/utils/supabase"
 
-const exemptedPaths = ["/home", "/auth", "/api/auth", "/api/price-empire", "/tos", "/privacy-policy"]
-
 export const config = {
     matcher: [
         /*
          * Match all request paths except for the ones starting with:
-         * - login (API routes)
+         * - home (home page)
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
          * - .(svg|png|jpg|jpeg|gif|webp) (image files)
          */
-        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+        "/((?!_next/static|_next/image|home|auth|api/price-empire|api/auth|tos|privacy-policy|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
     ],
 }
 
 export async function middleware(request: NextRequest) {
     const response = NextResponse.next()
-    const requestPath = request.nextUrl.pathname
     const supabase = getSupabaseClient()
     const { data: session } = await supabase.auth.getSession()
-    if (session?.session === null && !exemptedPaths.some((path) => requestPath.startsWith(path))) {
-        // Redirect to /home if not logged in and not on /home or /auth
+    if (session?.session === null) {
+        // Redirect to /home if not logged in
         const requestUrl = new URL(request.url)
         return NextResponse.redirect(requestUrl.origin + "/home")
     }
