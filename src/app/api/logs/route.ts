9c42193a5eab834c85b4a8c9988c1e47ce87cccf
Megham Garg <gargmegham3@gmail.com>
2024-06-08 13:54:07 +0530

    chore: logs should be ordered by create_at by default

--- src/app/api/logs/route.ts
+++ src/app/api/logs/route.ts
@@ -1,12 +1,12 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 
 import { getSupabaseClient } from "@/utils/supabase"
 
 import type { Log } from "@/types/database"
 
-export async function GET(request: NextRequest) {
+export async function GET() {
     const supabase = getSupabaseClient()
-    const { data, error } = await supabase.from("Logs").select("*")
+    const { data, error } = await supabase.from("Logs").select("*").order("created_at", { ascending: false })
     if (error) return NextResponse.json({ message: error.message }, { status: 500 })
     return NextResponse.json(data as Log[])
 }
