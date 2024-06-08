e5ba2c0a4939e024071c7a2f139e31bec0f4e284
Megham Garg <gargmegham3@gmail.com>
2024-06-08 06:28:12 +0530

    refactor: Remove unused logger import in cron.py

--- bot/cron.py
+++ bot/cron.py
@@ -4,7 +4,6 @@ import os
 import httpx
 from dotenv import load_dotenv
 
-from utils.logger import get_logger
 from utils.supabase import get_general_settings, get_supabase_client
 
 load_dotenv()
@@ -16,32 +15,18 @@ async def cron():
     """
     Send a post request to the server every 60 minutes using a cron job
     """
-    logger = get_logger("cron")
-    logger.info("ShieldPeer Cron service started")
-    try:
-        client = await get_supabase_client()
-        settings = await get_general_settings(client)
-        for setting in settings:
-            async with httpx.AsyncClient() as client:
-                await client.post(
-                    "https://shieldpeer.in/api/price-empire",
-                    headers={"key": SUPABASE_SERVICE_KEY},
-                    data={
-                        "user_id": setting["user_id"],
-                    },
-                )
-    except Exception as err:
-        logger.error(f"An error occurred in cron:", exc_info=True)
+    client = await get_supabase_client()
+    settings = await get_general_settings(client)
+    for setting in settings:
+        async with httpx.AsyncClient() as client:
+            await client.post(
+                "https://shieldpeer.in/api/price-empire",
+                headers={"key": SUPABASE_SERVICE_KEY},
+                data={
+                    "user_id": setting["user_id"],
+                },
+            )
 
 
 if __name__ == "__main__":
-    logger = get_logger("cron")
-    try:
-        logger.info("ShieldPeer Cron: service started")
-        asyncio.run(cron())
-    except KeyboardInterrupt:
-        logger.info("ShieldPeer Cron: service stopped")
-    except Exception as err:
-        logger.error(f"ShieldPeer Cron:", exc_info=True)
-    finally:
-        logger.info("ShieldPeer Cron: service stopped")
+    asyncio.run(cron())
