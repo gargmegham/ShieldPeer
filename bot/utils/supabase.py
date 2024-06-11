fca8e60e4263c3de6f455c228b9e7c82da18e1e5
Megham Garg <gargmegham3@gmail.com>
2024-06-11 09:10:33 +0530

    feat: Optimize insert_unique_logs function
    The insert_unique_logs function in supabase.py has been optimized to improve performance. The code now converts the "created_at" timestamp from a string to a datetime object before performing the comparison. This change ensures that the last log's timestamp is correctly calculated, allowing logs to be inserted if the last log was more than 1 hour ago. This optimization will help reduce unnecessary database queries and improve the overall efficiency of the function.

--- bot/utils/supabase.py
+++ bot/utils/supabase.py
@@ -126,7 +126,10 @@ async def insert_unique_logs(
         else:
             last_log = last_log.data[0]
             # if the last log was more than 1 hour ago
-            if (datetime.now() - last_log["created_at"]).total_seconds() > 3600:
+            created_at = datetime.strptime(
+                last_log["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
+            )
+            if (datetime.now() - created_at).total_seconds() > 3600:
                 logs_to_insert.append(log)
         unique_logs.add(log["message"])
     response = await client.table("Logs").insert(logs_to_insert).execute()
