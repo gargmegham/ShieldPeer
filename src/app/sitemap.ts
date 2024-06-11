9ddacb9e09666bf0c553a5efa88aa9379c7537bb
Megham Garg <95271253+gargmegham@users.noreply.github.com>
2024-06-11 20:17:11 +0530

    seo: fix sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,14 +6,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             // this is for landing page
-            url: `${config.domainName}/home`,
+            url: `https://${config.domainName}/home`,
             lastModified: new Date(),
             changeFrequency: "yearly",
             priority: 1,
         },
         {
             // this is for demo page
-            url: `${config.domainName}/demo`,
+            url: `https://${config.domainName}/demo`,
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 0.8,
