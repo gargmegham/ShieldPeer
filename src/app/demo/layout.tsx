96aafc42f5bef35eb11989c501bf2ddb97094d39
Megham Garg <95271253+gargmegham@users.noreply.github.com>
2024-06-11 16:28:55 +0530

    eslint: disabled react/no-unescaped-entities

--- src/app/demo/layout.tsx
+++ src/app/demo/layout.tsx
@@ -6,7 +6,7 @@ export default function DemoLayout({ children }: { children: React.ReactNode }) 
             {children}
             <div className="fixed top-2 w-full justify-center flex">
                 <div className="text-xs px-2 py-1 text-amber-600/80 bg-amber-600/20 max-w-xs text-center rounded-xl">
-                    This is a demo site. Please do not use real data.
+                    This is a demo site. Please don't use real data.
                 </div>
             </div>
         </>
