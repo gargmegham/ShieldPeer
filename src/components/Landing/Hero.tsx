3982902eff80595aef0be1a6921416627cda0d65
Megham Garg <gargmegham3@gmail.com>
2024-06-11 22:14:05 +0530

    feat: Add min-height to hero section on landing page

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
     return (
         <section
             id="hero"
-            className="h-screen w-full flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.1] relative overflow-hidden"
+            className="h-screen w-full flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.1] relative overflow-hidden min-h-[800px]"
         >
             <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
             <div className=" p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
