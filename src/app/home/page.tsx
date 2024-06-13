ddaba6fea85e59e400b3b62672a1860e2282c864
Megham Garg <gargmegham3@gmail.com>
2024-06-13 15:21:44 +0530

    refactor: hide github button on small screens

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
             <Link
                 href="https://github.com/gargmegham/ShieldPeer"
                 target="_blank"
-                className="right-4 p-2 bottom-4 border text-sm font-medium border-white/[0.2] text-white rounded-xl bg-black flex items-center justify-center fixed"
+                className="hidden right-4 p-2 bottom-4 border text-sm font-medium border-white/[0.2] text-white rounded-xl bg-black md:flex items-center justify-center fixed"
             >
                 <GitHubLogoIcon className="w-5 h-5 mr-2" />
                 <span>Star Us On Github</span>
